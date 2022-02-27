82b591d68af9d41ff8878dec75dbea158f9aaaf1
atadagg <98890853+atadagg@users.noreply.github.com>
2022-02-27 17:52:12 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:flutterapp/sigmakediapp/generatedmainpagewidget/GeneratedMainPageWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedproductcataloguewidget/GeneratedProductCatalogueWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedservicecataloguewidget/GeneratedServiceCatalogueWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedservicecategorypagewidget/GeneratedServiceCategoryPageWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedframe43widget1/GeneratedFrame43Widget1.dart';
-import 'package:flutterapp/sigmakediapp/generatedproductpagewidget/GeneratedProductPageWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedcontactspagewidget/GeneratedContactsPageWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedmessagepagewidget/GeneratedMessagePageWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedsatpagewidget/GeneratedSatPageWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedilanurunwidget/GeneratedIlanUrunWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedilanhizmetwidget/GeneratedIlanHizmetWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedprofilepagewidget/GeneratedProfilePageWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedlearnwidget/GeneratedLearnWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedhubwidget/GeneratedHubWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedloginpagewidget/GeneratedLoginPageWidget.dart';
-import 'package:flutterapp/sigmakediapp/generatedvideowidget/GeneratedVideoWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedmainpagewidget/GeneratedMainPageWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedproductcataloguewidget/GeneratedProductCatalogueWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedservicecataloguewidget/GeneratedServiceCatalogueWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedservicecategorypagewidget/GeneratedServiceCategoryPageWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedproductpagewidget/GeneratedProductPageWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedcontactspagewidget/GeneratedContactsPageWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedmessagepagewidget/GeneratedMessagePageWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedsatpagewidget/GeneratedSatPageWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedilanurunwidget/GeneratedIlanUrunWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedilanhizmetwidget/GeneratedIlanHizmetWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedprofilepagewidget/GeneratedProfilePageWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedlearnwidget/GeneratedLearnWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedhubwidget/GeneratedHubWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedloginpagewidget/GeneratedLoginPageWidget.dart';
+import 'package:flutterapp/sigma_20kediiapp/generatedvideowidget/GeneratedVideoWidget.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(sigmakediApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureIntiliaized();
+  await Firebase.initializeApp();
+  runApp(sigma_20kediiApp());
 }
 
-class sigmakediApp extends StatelessWidget {
+class sigma_20kediiApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/GeneratedLoginPageWidget',
-      routes: {
-        '/GeneratedMainPageWidget': (context) => GeneratedMainPageWidget(),
-        '/GeneratedProductCatalogueWidget': (context) =>
-            GeneratedProductCatalogueWidget(),
-        '/GeneratedServiceCatalogueWidget': (context) =>
-            GeneratedServiceCatalogueWidget(),
-        '/GeneratedServiceCategoryPageWidget': (context) =>
-            GeneratedServiceCategoryPageWidget(),
-        '/GeneratedFrame43Widget1': (context) => GeneratedFrame43Widget1(),
-        '/GeneratedProductPageWidget': (context) =>
-            GeneratedProductPageWidget(),
-        '/GeneratedContactsPageWidget': (context) =>
-            GeneratedContactsPageWidget(),
-        '/GeneratedMessagePageWidget': (context) =>
-            GeneratedMessagePageWidget(),
-        '/GeneratedSatPageWidget': (context) => GeneratedSatPageWidget(),
-        '/GeneratedIlanUrunWidget': (context) => GeneratedIlanUrunWidget(),
-        '/GeneratedIlanHizmetWidget': (context) => GeneratedIlanHizmetWidget(),
-        '/GeneratedProfilePageWidget': (context) =>
-            GeneratedProfilePageWidget(),
-        '/GeneratedLearnWidget': (context) => GeneratedLearnWidget(),
-        '/GeneratedHubWidget': (context) => GeneratedHubWidget(),
-        '/GeneratedLoginPageWidget': (context) => GeneratedLoginPageWidget(),
-        '/GeneratedVideoWidget': (context) => GeneratedVideoWidget(),
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/GeneratedLoginPageWidget',
+        routes: {
+          '/GeneratedMainPageWidget': (context) => GeneratedMainPageWidget(),
+          '/GeneratedProductCatalogueWidget': (context) =>
+              GeneratedProductCatalogueWidget(),
+          '/GeneratedServiceCatalogueWidget': (context) =>
+              GeneratedServiceCatalogueWidget(),
+          '/GeneratedServiceCategoryPageWidget': (context) =>
+              GeneratedServiceCategoryPageWidget(),
+          '/GeneratedProductPageWidget': (context) =>
+              GeneratedProductPageWidget(),
+          '/GeneratedContactsPageWidget': (context) =>
+              GeneratedContactsPageWidget(),
+          '/GeneratedMessagePageWidget': (context) =>
+              GeneratedMessagePageWidget(),
+          '/GeneratedSatPageWidget': (context) => GeneratedSatPageWidget(),
+          '/GeneratedIlanUrunWidget': (context) => GeneratedIlanUrunWidget(),
+          '/GeneratedIlanHizmetWidget': (context) => GeneratedIlanHizmetWidget(),
+          '/GeneratedProfilePageWidget': (context) =>
+              GeneratedProfilePageWidget(),
+          '/GeneratedLearnWidget': (context) => GeneratedLearnWidget(),
+          '/GeneratedHubWidget': (context) => GeneratedHubWidget(),
+          '/GeneratedLoginPageWidget': (context) => GeneratedLoginPageWidget(),
+          '/GeneratedVideoWidget': (context) => GeneratedVideoWidget(),
       },
     );
-  }
+  );
 }
+
